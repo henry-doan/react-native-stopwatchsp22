2e2b03da2f4b1fc5b4d5bb72b3ea62611b01c9f7
Henry Doan <henrydoan96@gmail.com>
2022-04-25 19:04:42 -0600

    end of stopwatch

---
+++ App.js
@@ -0,0 +1,24 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+} from 'react-native';
+import StopWatch from './src/screens/StopWatch';
+
+const App = () => {
+
+  return (
+    <SafeAreaView>
+      <StopWatch />
+    </SafeAreaView>
+  );
+};
+
+export default App;
